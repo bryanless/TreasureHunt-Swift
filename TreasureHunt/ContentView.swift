d39a06752b83d7aa226730d9804b33d426849003
Kenny Jinhiro <jxnhiro@gmail.com>
2023-08-02 09:43:09 +0700

    Sticking Object in Camera

--- TreasureHunt/ContentView.swift
+++ TreasureHunt/ContentView.swift
@@ -20,6 +20,23 @@ struct ARViewContainer: UIViewRepresentable {
         
         let arView = ARView(frame: .zero)
         
+        
+        let box = ModelEntity(
+              mesh: MeshResource.generateBox(size: 0.075),
+              materials: [SimpleMaterial(color: .red, isMetallic: true)]
+            )
+
+        let cameraAnchor = AnchorEntity(.camera)
+        cameraAnchor.addChild(box)
+        arView.scene.addAnchor(cameraAnchor)
+
+        // Move the box in front of the camera slightly, otherwise
+            // it will be centered on the camera position and we will
+            // be inside the box and not be able to see it
+        box.transform.translation = [0, -0.25, -0.75]
+        //Rotation downwards in X for 45 degrees
+        box.transform.rotation = simd_quatf(angle: 0.785, axis: SIMD3<Float>(1,0,0))
+        
         // Load the "Box" scene from the "Experience" Reality File
         let boxAnchor = try! Experience.loadBox()
         
