1a2d71067db6257925972a5bbe9f1fa672ad8df7
Bryan <71095749+bryanless@users.noreply.github.com>
2023-08-04 11:48:05 +0700

    Resolve conflict

--- TreasureHunt/ContentView.swift
+++ TreasureHunt/ContentView.swift
@@ -8,68 +8,71 @@
 import SwiftUI
 import RealityKit
 
-struct ContentView : View {
-    @StateObject var arVM = ARViewModel()
-    
+struct ContentView: View {
+    @StateObject var locationVM = LocationViewModel()
+
     var body: some View {
         ZStack(alignment: .bottom) {
-            ARViewContainer(arVM: arVM).edgesIgnoringSafeArea(.all)
-            if let location = arVM.currentLocation {
+            ARViewContainer().edgesIgnoringSafeArea(.all)
+            if let location = locationVM.currentLocation {
                 VStack {
                     Text("Location Latitude: \(location.coordinate.latitude)")
-                    Text(arVM.messageText)
+                    Text(locationVM.messageText)
                 }
                 .padding()
+                .frame(height: 100)
+                .frame(maxWidth: .infinity)
             }
         }
     }
 }
 
 struct ARViewContainer: UIViewRepresentable {
-    //Add MotionManager
+    // Add MotionManager
     @StateObject var motion = MotionManager()
-    //Anchor from Camera
+    // Anchor from Camera
     let cameraAnchor = AnchorEntity(.camera)
-    
-    var arVM: ARViewModel
+
     func makeUIView(context: Context) -> ARView {
-        //Add ARView to call
+        // Add ARView to call
         let arView = ARView(frame: .zero)
-        //Add Metal Detector from Models in Bundle
+        // Add Metal Detector from Models in Bundle
         let path = Bundle.main.path(forResource: "metal_detector", ofType: "usdz")!
-        //Add URL Path from Bundle
+        // Add URL Path from Bundle
         let url = URL(fileURLWithPath: path)
-        //Load Entity to Metal Detector
+        // Load Entity to Metal Detector
         let metalDetector = try? Entity.load(contentsOf: url)
         metalDetector!.name = "metalDetector"
-        //Add Metal Detector Right On Camera
+        // Add Metal Detector Right On Camera
         cameraAnchor.addChild(metalDetector!)
-        //Add Camera Anchor to the Scene after adding child
+        // Add Camera Anchor to the Scene after adding child
         arView.scene.addAnchor(cameraAnchor)
         // Move Metal Detector Downwards and Front
         metalDetector!.transform.translation = [0, -1.75, -3.15]
-        //Rotation downwards in X for 90 degrees
-        metalDetector!.transform.rotation *= simd_quatf(angle: 1.5708, axis: SIMD3<Float>(0,1,0))
-        //Rotation downwards in z for 50 degrees
-        metalDetector!.transform.rotation *= simd_quatf(angle: 0.959931, axis: SIMD3<Float>(0,0,1))
-        
+        // Rotation downwards in X for 90 degrees
+        metalDetector!.transform.rotation *= simd_quatf(angle: 1.5708, axis: SIMD3<Float>(0, 1, 0))
+        // Rotation downwards in z for 50 degrees
+        metalDetector!.transform.rotation *= simd_quatf(angle: 0.959931, axis: SIMD3<Float>(0, 0, 1))
+
         return arView
     }
-    
+
     func updateUIView(_ uiView: ARView, context: Context) {
-        if(uiView.scene.anchors[0].children[0].transform.rotation.real < 0.6268) || (uiView.scene.anchors[0].children[0].transform.rotation.real > 0.62758){
-            uiView.scene.anchors[0].children[0].transform.rotation *= simd_quatf(angle: Float(motion.x * 0.00002125), axis: SIMD3<Float>(1,0,0))
-        }
-        else{
-            uiView.scene.anchors[0].children[0].transform.rotation *= simd_quatf(angle: Float(motion.x * 0.00002125), axis: SIMD3<Float>(1,0,0))
+        if uiView.scene.anchors[0].children[0].transform.rotation.real < 0.6268
+            || uiView.scene.anchors[0].children[0].transform.rotation.real > 0.62758 {
+            uiView.scene.anchors[0].children[0].transform.rotation *=
+            simd_quatf(angle: Float(motion.x * 0.00002125), axis: SIMD3<Float>(1, 0, 0))
+        } else {
+            uiView.scene.anchors[0].children[0].transform.rotation *=
+            simd_quatf(angle: Float(motion.x * 0.00002125), axis: SIMD3<Float>(1, 0, 0))
             uiView.scene.anchors[0].children[0].transform.translation += [Float(motion.x * 0.0005), 0, 0]
         }
     }
-    
+
 }
 
 #if DEBUG
-struct ContentView_Previews : PreviewProvider {
+struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
--- TreasureHunt/LocationViewModel.swift
+++ TreasureHunt/LocationViewModel.swift
@@ -27,8 +27,7 @@ enum MetalDetectorState {
         }
     }
 }
-class ARViewModel: ObservableObject {
-    @Published private var model: ARModel = ARModel()
+class LocationViewModel: ObservableObject {
     private let locationManager = LocationManager.instance
     private var cancellables = Set<AnyCancellable>()
     @Published var currentLocation: CLLocation?
@@ -39,9 +38,7 @@ class ARViewModel: ObservableObject {
     init() {
         addSubscribers()
     }
-    var arView: ARView {
-        model.arView
-    }
+
     private func addSubscribers() {
         locationManager.$location
             .combineLatest(locationManager.$initialLocation)
@@ -148,32 +145,6 @@ class ARViewModel: ObservableObject {
                 // TODO: SPAWN TREASURE HERE, UPDATE TREASURE HAS SPAWNED STATE
             }
         }
-    }
-
-    // MARK: AR Functions
-    func spawnMetalDetector() {
-        let path = Bundle.main.path(forResource: "metal_detector", ofType: "usdz")!
-        let url = URL(fileURLWithPath: path)
-
-        let metalDetector = try? Entity.load(contentsOf: url)
-
-        //        let metalDetector = ModelEntity(
-        //              mesh: MeshResource.generateBox(size: 0.075),
-        //              materials: [SimpleMaterial(color: .red, isMetallic: true)]
-        //            )
-
-        let cameraAnchor = AnchorEntity(.camera)
-
-        // Move the box in front of the camera slightly, otherwise
-        // it will be centered on the camera position and we will
-        // be inside the box and not be able to see it
-        metalDetector!.transform.translation = [0, -1.75, -3.15]
-        // Rotation downwards in X for 45 degrees
-        metalDetector!.transform.rotation *= simd_quatf(angle: 1.5708, axis: SIMD3<Float>(0, 1, 0))
-        metalDetector!.transform.rotation *= simd_quatf(angle: 0.959931, axis: SIMD3<Float>(0, 0, 1))
-
-        cameraAnchor.addChild(metalDetector!)
-        model.arView.scene.addAnchor(cameraAnchor)
     }
 
 }
