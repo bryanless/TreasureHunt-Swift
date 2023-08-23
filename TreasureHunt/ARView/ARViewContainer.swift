550f586d6308e5139dde9b4bb8152965ee7e637c
Bryan <71095749+bryanless@users.noreply.github.com>
2023-08-23 10:33:53 +0700

    Fix metal detector jumping transition

--- TreasureHunt/ARView/ARViewContainer.swift
+++ TreasureHunt/ARView/ARViewContainer.swift
@@ -25,17 +25,16 @@ struct ARViewContainer: UIViewRepresentable {
             switch result {
             case .success(let metalDetector):
                 metalDetector.name = "metalDetector"
-                // Add Metal Detector Right On Camera
-                self.cameraAnchor.addChild(metalDetector)
-                // Add Camera Anchor to the Scene after adding child
-                self.gameViewModel.arView?.scene.addAnchor(self.cameraAnchor)
                 // Move Metal Detector Downwards and Front
                 metalDetector.transform.translation = [0, -1.75, -3.15]
                 // Rotation downwards in X for 90 degrees
                 metalDetector.transform.rotation *= simd_quatf(angle: 1.5708, axis: SIMD3<Float>(0, 1, 0))
                 // Rotation downwards in z for 50 degrees
                 metalDetector.transform.rotation *= simd_quatf(angle: 0.959931, axis: SIMD3<Float>(0, 0, 1))
-
+                // Add Metal Detector Right On Camera
+                self.cameraAnchor.addChild(metalDetector)
+                // Add Camera Anchor to the Scene after adding child
+                self.gameViewModel.arView?.scene.addAnchor(self.cameraAnchor)
             case .failure(let error):
                 debugPrint(error.localizedDescription)
             }
