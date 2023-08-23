c7ad7d4ac506f72b4e4c281050c8ebdb2ed74fe6
SumahanZ <83936130+SumahanZ@users.noreply.github.com>
2023-08-23 08:30:07 +0700

    pushed

--- TreasureHunt/Viewmodels/Game/GameViewModel+Location.swift
+++ TreasureHunt/Viewmodels/Game/GameViewModel+Location.swift
@@ -117,7 +117,7 @@ extension GameViewModel {
         let treasures = generateTreasureLocationsWithinRegion(
             initialLocation: initialLocation,
             region: gameArea,
-            treasureAmount: 3,
+            treasureAmount: 1,
             distanceFromInitial: 15,
             distanceBetweenTreasures: 12)
         return treasures
